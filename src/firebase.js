04b11e73b7468b6d2f48b238ffee6210b1695ade
Kushan7 <carchana813@gmail.com>
2025-08-18 23:55:21 +0530

    Edited index

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,15 @@
-// Import the functions you need from the SDKs you need
-
-
 // src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"; // Semicolon was added here
+import { getStorage } from "firebase/storage";
 
+// Your web app's Firebase configuration with corrected storageBucket
 const firebaseConfig = {
   apiKey: "AIzaSyA_ug0BeWXXaJ0bub6IlJm-4MQbKNFi1bM",
   authDomain: "easylib-c9ba3.firebaseapp.com",
-  databaseURL: "https://easylib-c9ba3-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "easylib-c9ba3",
-  storageBucket: "easylib-c9ba3.firebasestorage.app",
+  storageBucket: "easylib-c9ba3.appspot.com", // This line was corrected
   messagingSenderId: "457259066657",
   appId: "1:457259066657:web:117f9ab6525333355a4a60",
   measurementId: "G-CQE7LC86GV"
